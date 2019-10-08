b502c71c24fb43e09dbda030dfa901ef67d78f0f
zhangjun <790282561@qq.com>
2019-10-09 00:25:07 +0800

    制作了总的押韵韵脚，并制作了云图

--- 词频云图.py
+++ 词频云图.py
@@ -3,23 +3,22 @@ import os
 import re
 
 import jieba
-import wordcloud
+from wordcloud import WordCloud
 
 
 def get_rhyme(f_name):
     f = open('./lyrics/' + f_name, 'r', encoding='UTF-8')
-    print(f_name + '写入开始')
     text = f.readlines()
     f.close()
 
-    # 处理开头
+    '''处理开头'''
     for m, n in enumerate(text):
         if '编曲：' in n:
             lyric_drop_head = text[m + 1:]
         elif '评论数' in n:
             lyric_drop_head = text[m + 1:]
 
-    # 处理结尾
+    '''处理结尾'''
     for o, p in enumerate(lyric_drop_head):
         if '制作人：陈令韬/欧智\n' in p:
             lyric_text_tail = lyric_drop_head[:o]
@@ -36,25 +35,38 @@ def get_rhyme(f_name):
         else:
             lyric_text_tail = lyric_drop_head
 
-    # 处理中间段
-    lyric_text = []
-    re_text = r'（[\u4E00-\u9FA5\w]+）|\([\u4E00-\u9FA5\w]+\)|（[\u4E00-\u9FA5\w]+\)'
-    re_brackets = re.compile(re_text)
+    '''处理中间段'''
+    # 处理掉空列表
     if '\n' in lyric_text_tail:
         while '\n' in lyric_text_tail:
             lyric_text_tail.remove('\n')
+    # 处理掉演唱者及冒号的行列
+    del_list = []
+    for a in lyric_text_tail:
+        if '：' in a:
+            del_list.append(a)
+        elif ':' in a:
+            del_list.append(a)
+    lyric_text_tail = list(set(lyric_text_tail) - set(del_list))
+    # 处理掉换行符、特殊符号, 并分行
+    lyric_text = []
+    re_text = r'（[\u4E00-\u9FA5\w\s]+）|\([\u4E00-\u9FA5\w\s]+\)|（[\u4E00-\u9FA5\w\s]+\)|\([\u4E00-\u9FA5\w\s]+）'
+    re_brackets = re.compile(re_text)
     for i in lyric_text_tail:
         i = i.replace('\n', '')
-        j = re.split(re_brackets, i)
-        while '' in j:
-            j.remove('')
+        j = re.sub(re_brackets, '《', i)
+        # while '' in j:
+        #     j.remove('')
         j = ''.join(itertools.chain(j))
-        for k in j.split(' '):
-            lyric_text.append(k)
-    for x1, x2 in enumerate(lyric_text):
-        if '：' in x2:
-            del lyric_text[x1]
-
+        if '《' in j:
+            j = j.replace('《', '').replace('》', '')
+        if '”' in j:
+            j = j.replace('“', '').replace('”', '')
+        lyric_text.append(j)
+    #设置分词动态字典
+    cut_dict = ('飙翻', 'A等货')
+    for cut in cut_dict:
+        jieba.add_word(cut, freq=100)
     # 分词写入文件
     for words_cut in lyric_text:
         words = list(jieba.cut(words_cut, cut_all=False))
@@ -63,6 +75,17 @@ def get_rhyme(f_name):
                 f.write(words[-1] + ',')
     print(f_name + '写入完成')
 
-def word_wind
-for f_name in os.listdir('./lyrics'):
-    get_rhyme(f_name)
+
+def word_cloud():
+    f = open('rhyme_word.txt', 'r', encoding='UTF-8').read()
+    # f = [i for i in f if i != ' ']
+    wordcloud = WordCloud(background_color='white',
+                          width=800,
+                          height=600,
+                          margin=2,
+                          font_path='simsun.ttc')
+    wordcloud.generate(f)
+    wordcloud.to_file('rhyme_word.png')
+# for f_name in os.listdir('./lyrics'):
+#     get_rhyme(f_name)
+word_cloud()
